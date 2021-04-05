f6f8e64b9e46e30c12c8749f919a6696a2c3b6f6
impratik <pratikmpsc@gmail.com>
2021-04-06 02:26:58 +0530

    second important commit

--- src/components/Home/Projects/ProjectItem.js
+++ src/components/Home/Projects/ProjectItem.js
@@ -1,19 +1,21 @@
 import React from 'react'
-import Card from '../../../UIElements/Card';
+// import Card from '../../../UIElements/Card';
 import './ProjectItem.css';
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
 const ProjectItem = props => {
+
+   
     return (
         <li className="list-item">
             <Link to={`/${props.name}`}>
-            <Card>
-                <div>
-                    <img src={props.image} alt={props.name} />
+                <div> 
+                    <img src={props.image} alt={props.name} className="item-image" />
                 </div>
-                <div>
+                
+                <div className="item-desc">
                     <h4> {props.description} </h4>
                 </div>
-            </Card>
+            
             </Link>
         </li>
     )
